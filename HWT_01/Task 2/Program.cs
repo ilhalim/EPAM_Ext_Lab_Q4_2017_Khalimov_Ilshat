4d29c131aaedceb9e8a0cd672feb7d6b091320b7
Ilshat <33999485+ilhalim@users.noreply.github.com>
2017-11-28 17:46:28 +0400

    Add files via upload

---
+++ HWT_01/Task 2/Program.cs
@@ -0,0 +1,52 @@
+﻿/* Дано действительное число h.Выяснить, имеет ли уравнение a*x^2+b*x+c=0 действительные корни
+Пграмма должна выводить пользователю промежуточные вычисления
+(например, a, b, c и дискриминант (если вычисляли при помощи него) и корни (если есть)). */
+namespace Task_02
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            double h, a, b, c, discrim, x1, x2, temp1, temp2;
+            Console.WriteLine("Please, write number h ");
+            h = double.Parse(Console.ReadLine());
+            temp1 = Math.Abs(Math.Sin(8 * h) + 17);
+            temp2 = 1 - (Math.Sin(4 * h) * Math.Cos(Math.Pow(h, 2) + 18));
+            a = Math.Sqrt(temp1 / (temp2 * temp2));
+            temp1 = Math.Tan(a * Math.Pow(h, 2));
+            temp2 = Math.Sin(a * h);
+            b = 1 - Math.Sqrt(3 / (3 + Math.Abs(temp1 - temp2)));
+            temp1 = a * Math.Pow(h, 2) * Math.Sin(b * h);
+            temp2 = b * Math.Pow(h, 3) * Math.Cos(a * h);
+            c = temp1 + temp2;
+            discrim = Math.Pow(b, 2) - (4 * a * c);
+            if (discrim < 0)
+            {
+                Console.WriteLine("Уравнение не имеет действительных корней");
+            }
+            else if (discrim > 0)
+            {
+                x1 = (-b + Math.Sqrt(discrim)) / (2 * a);
+                x2 = (-b - Math.Sqrt(discrim)) / (2 * a);
+                Console.WriteLine("The equation has 2 roots");
+                Console.WriteLine("x1={0},x2={1}", x1, x2);
+                }
+            else
+            {
+                x1 = -1 * b / 2 * a;
+                Console.WriteLine("The equation has 1 root");
+                Console.WriteLine("x={0}", x1);
+            }
+
+            Console.WriteLine("Number а={0}, number b={1}, number c ={2}", a, b, c);
+            Console.WriteLine("discriminant equal to {0}", discrim);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HWT_01/Task_01/Program.cs
@@ -0,0 +1,136 @@
+﻿/*Написать консольное приложение, которое проверяет принадлежность точки заштрихованной области.  Пользователь вводит координаты точки (x; y)
+и выбирает букву графика (a-к). В консоли должно высветиться сообщение: «Точка [(x; y)] принадлежит фигуре [г]». */
+namespace Task_01
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y;
+            string grafick;
+            bool inGrafick = false;
+
+            Console.WriteLine("Please,write coordinate X");
+            x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please,write coordinate Y");
+            y = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please,write type of figure");
+            grafick = Console.ReadLine();
+            double r;
+            r = Math.Sqrt((x * x) + (y * y));
+            switch (grafick)
+            {
+                case "a":
+                    if (r <= 1)
+                    {
+                       inGrafick = true;
+                    }
+
+                    break;
+                case "b":
+                    if ((r <= 1) && (r >= 0.5))
+                    {
+                        inGrafick = true;
+                    }
+
+                    break;
+                case "c":
+                    if ((Math.Abs(x) <= 1) && (Math.Abs(y) <= 1))
+                    {
+                       inGrafick = true;
+                    }
+
+                    break;
+                case "d":
+                    if ((y >= Math.Abs(x) - 1) && (y <= (-1 * Math.Abs(x)) + 1))
+                    {
+                        inGrafick = true;
+                    }
+
+                    break;
+                case "e":
+                    if ((y >= (2 * Math.Abs(x)) - 1) && (y <= (-2 * Math.Abs(x)) + 1))
+                    {
+                        inGrafick = true;
+                    }
+
+                    break;
+                case "f":
+                    if (x >= 0)
+                    {
+                        if (r <= 1)
+                        {
+                            inGrafick = true;
+                        }
+                    }
+                    else
+                        if ((y <= 0.5 * x + 2) && (y >= -0.5 * x + 2))
+                    {
+                        inGrafick = true;
+                    }
+
+                    break;
+                case "g":
+                    if ((y <= (-2 * Math.Abs(x)) + 2) && (y >= -1))
+                    {
+                        inGrafick = true;
+                    }
+
+                    break;
+                case "h":
+                    if ((y <= Math.Abs(x)) && (y >= -2) && (Math.Abs(x) <= 1))
+                    {
+                        inGrafick = true;
+                    }
+
+                    break;
+                case "i":
+                    if (y <= 0)
+                    {
+                        if ((y <= ((2 / 3) * x) + (1 / 3)) && (y >= ((1 / 3) * x) - (1 / 3)))
+                        {
+                            inGrafick = true;
+                        }
+                    }
+                    else
+                    {
+                        if ((y <= 2 / 3 * x + 1 / 3) && (y <= -1 * x))
+                        {
+                            inGrafick = true;
+                        }
+                    }
+
+                    break;
+                case "к":
+                    if ((y >= 1) || (y >= Math.Abs(x)))
+                    {
+                        inGrafick = true;
+                    }
+
+                    break;
+                default:
+                    Console.WriteLine("Error, choosen figure {0} is not specified, please choose from (a-i)", grafick);
+                    Console.ReadLine();
+                    Environment.Exit(0);
+                    break;
+                    }
+
+            if (inGrafick)
+            {
+                Console.WriteLine("«The point ({0};{1}) lies on the figure {2}».", x, y, grafick);
+            }
+            else
+            {
+                Console.WriteLine("The point ({0};{1}) is not lie on the figure {2}.", x, y, grafick);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
