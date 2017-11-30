e67325bd07733613b64f6b39fd12b953b4952d28
Ilshat <33999485+ilhalim@users.noreply.github.com>
2017-11-30 18:50:19 +0400

    Add files via upload

---
+++ HWT_02/Task_01/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace Task_01
+{
+    using System;
+        
+    public class Program
+    {
+        public static int Area(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static void Main(string[] args)
+        {
+            int a, b, s;
+            while (true)
+            {
+                while (true)
+                {
+                    Console.WriteLine("Please write a");
+                    if (int.TryParse(Console.ReadLine(), out a))
+                    {
+                        if (a < 1)
+                        {
+                            Console.WriteLine("Error.Please write correct value of a ");
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error.Please write correct value of a ");
+                    }
+                }
+
+                while (true)
+                {
+                    Console.WriteLine("Please write b");
+                    if (int.TryParse(Console.ReadLine(), out b))
+                    {
+                        if (a < 1)
+                        {
+                            Console.WriteLine("Error.Please write correct value of b ");
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error.Please write correct value of b ");
+                    }
+                }
+
+               s = Area(a, b);
+                Console.WriteLine("The rectangle area = {0} ", s);
+                Console.WriteLine("For close program use button Escape(Esp)");
+                ConsoleKeyInfo c = Console.ReadKey();
+                if (c.Key == ConsoleKey.Escape)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
