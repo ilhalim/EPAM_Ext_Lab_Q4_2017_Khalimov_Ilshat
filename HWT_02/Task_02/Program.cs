1283a0b3c6c498cebe91a50f4249e8ce1c3c63d4
Ilshat <33999485+ilhalim@users.noreply.github.com>
2017-11-30 18:44:10 +0400

    Add files via upload

---
+++ HWT_02/Task_02/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Task_02
+{
+    using System;    
+
+   public class Program
+    {
+        public static void OutoutStars(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+         }
+
+        public static void Main(string[] args)
+        {
+          while (true)
+            {
+                int n;
+                while (true)
+                {                   
+                    Console.WriteLine("Please write n");
+                    if (int.TryParse(Console.ReadLine(), out n))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error.Please write correct value of n ");
+                    }
+                  }   
+                                      
+                    OutoutStars(n);                
+                Console.WriteLine("For close program use button Escape(Esp)");
+                ConsoleKeyInfo c = Console.ReadKey();
+                if (c.Key == ConsoleKey.Escape)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ HWT_02/Task_03/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Task_03
+{
+    using System;
+
+    public class Program
+    {
+        public static void StarsInTreangle(int n)
+        {
+            int[] treangle = new int[(2 * n) - 1];
+        
+             for (int i = 0; i < n; i++)
+             {
+                treangle[n - i - 1] = 1;
+                treangle[n + i - 1] = 1;
+                for (int j = 0; j < ((2 * n) - 1); j++)
+                 {                    
+                    if (treangle[j ] == 1)
+                     {
+                     Console.Write("*");
+                     }
+                     else
+                     {
+                     Console.Write(" ");
+                     }                     
+                 }
+
+                Console.WriteLine();
+             } 
+        }
+
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                int n;
+                while (true)
+                {
+                    Console.WriteLine("Please write n");
+                    if (int.TryParse(Console.ReadLine(), out n))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error.Please write correct value of n ");
+                    }
+                }
+
+                StarsInTreangle(n);
+                Console.WriteLine("For close program use button Escape(Esp)");
+                ConsoleKeyInfo c = Console.ReadKey();
+                if (c.Key == ConsoleKey.Escape)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
